1dfb0845d15a03ff4760d0cd03d4b27eb49afd1f
maxisme <max@maxis.me>
2016-03-18 17:55:23 +0000

    -

---
+++ terms.php
@@ -0,0 +1,34 @@
+<?php 
+
+?>
+<!doctype html>
+<html>
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+<meta name="viewport" content="initial-scale=1, maximum-scale=1">
+<meta name="author" content="Maximilian Mitchell">
+<link href='https://fonts.googleapis.com/css?family=Abel' rel='stylesheet' type='text/css'>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+<style>
+*{
+	font-family: 'Abel', sans-serif;
+	font-size:30px;
+	margin:0px;
+	padding:0px;
+}
+</style>
+
+<?php include_once("analyticstracking.php") ?>
+</head>
+
+<body>
+<h1>Terms of Service</h1>
+<p>By using the "Pulverize" web site ("Service") you are agreeing to be bound by the following terms and conditions ("Terms of Service").</p>
+<br>
+<p>Use of the Service is at your own risk. The Service is provided on an "AS IS" and "AS AVAILABLE" basis without any representation or endorsement made and without warranty of any kind whether expressed or implied, including but not limited to the implied warranties of satisfactory quality, fitness for a particular purpose, non-infringement, compatibility, security and accuracy.</p><br>
+<p>To the extent permitted by law, the Service, will not be liable for any indirect or consequential loss or damage whatsoever (including without limitation, loss of business, opportunity, data, profits) arising out of or in connection with the use of the Service.</p><br>
+<p>Content that is unlawful, offensive, threatening, libelous, defamatory, pornographic, obscene or otherwise objectionable or violates any party's intellectual property or these Terms of Service is prohibited.</p><br>
+<p>The failure of the Service to exercise or enforce any right or provision of the Terms of Service shall not constitute a waiver of such right or provision. The Terms of Service constitutes the entire agreement between you and the Service and govern your use of the Service, superseding any prior agreements between you and the Service (including, but not limited to, any prior versions of the Terms of Service).</p>
+</body>
+</html>
